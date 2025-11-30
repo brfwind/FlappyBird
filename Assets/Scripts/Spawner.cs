83e9f72a792a2ef447ca5dedfcbacdfb809b0014
brfwind <19172405320@163.com>
2025-11-30 10:28:43 +0800

    fix:解决屏幕适配问题，修复停止生成障碍物bug
    利用Canvas上的Canvas Scaler组件，设置了游戏设计分辨率，并且在Build Settings里设置了固定分辨率，不能自己调整
    
    障碍物生成bug的解决详见Spawner脚本里的新增注释，涉及协程和Invoke

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -17,16 +17,22 @@ public class Spawner : MonoBehaviour
     {
         if(BirdController.isDead)
         {
-            StopCoroutine(startPush());
+            CancelInvoke("Spawn");
         }
     }
 
+    //这里使用协程
+    //是因为要等待isStart变为true后，开启重复生成障碍物，所以需要一直监测isStart
+    //但是又不能放在周期函数里，因为InvokeRepeating只能执行一次
+    //所以用这个协程，在Start()就开启此协程，此后直到isStart = true，才执行InvokeRepeating
     IEnumerator startPush()
     {
         yield return new WaitUntil(() => UIManager.isStart);
 
-        InvokeRepeating("Spawn",0f,Random.Range(2f,2f));
+        InvokeRepeating("Spawn",0f,2f);
     }
+    //此外InvokeRepeating相当于启动了一个重复执行Spawn方法的Invoke延时调用
+    //关闭协程是不顶用的，得关闭这个Invoke：CancelInvoke("Spawn")
 
     private void Spawn()
     {
